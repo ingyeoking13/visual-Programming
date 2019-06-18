2b3d88c9926550d5bb5259f0a9dd0aaddc78ebd2
ingyeoking13 <ingyeoking13@gmail.com>
2019-06-19 03:16:10 +0900

    hi

--- of_v0.10.1_vs2017_release/apps/myApps/20190617/src/main.cpp
+++ of_v0.10.1_vs2017_release/apps/myApps/20190617/src/main.cpp
@@ -2,12 +2,3 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
-	ofSetupOpenGL(720,720,OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
-
-}
